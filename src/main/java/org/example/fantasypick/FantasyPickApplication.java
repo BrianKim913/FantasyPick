8a1b39da18b978b376ddb3f0240ab620b666f66e
Kim Bum Soo <bumsookim333@naver.com>
2025-08-25 23:59:31 +0900

    Update .gitignore

---
+++ src/main/java/org/example/fantasypick/FantasyPickApplication.java
@@ -0,0 +1,13 @@
+package org.example.fantasypick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyPickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyPickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fantasypick/FantasyPickApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fantasypick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyPickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

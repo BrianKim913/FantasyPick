8339ff164c361e6001fd0ccf389f5a9627a1cdf4
Kim Bum Soo <bumsookim333@naver.com>
2025-08-26 00:27:54 +0900

    feat: 프로젝트 스켈레톤 생성

--- src/main/java/org/example/fantasypick/application/mainapp/FantasyPickApplication.java
+++ src/main/java/org/example/fantasypick/application/mainapp/FantasyPickApplication.java
@@ -1,4 +1,4 @@
-package org.example.fantasypick;
+package org.example.fantasypick.application.mainapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

a980d51fdf0432f881c19c2d85771c0e106b06b3
Kim Bum Soo <bumsookim333@naver.com>
2025-08-26 03:38:15 +0900

    feat: 애플리케이션 config 생성

---
+++ src/main/java/org/example/fantasypick/application/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package org.example.fantasypick.application.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.example.fantasypick")
+public class ApplicationConfig {
+    
+}

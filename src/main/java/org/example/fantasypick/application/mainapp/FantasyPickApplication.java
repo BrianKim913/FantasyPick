c6a14bc91128f71852f14a58e3859830c4fce3b0
Kim Bum Soo <bumsookim333@naver.com>
2025-08-26 03:36:36 +0900

    feat: FantasyPickApplication 및 베이스 패키지 스캐닝 추가

--- src/main/java/org/example/fantasypick/application/mainapp/FantasyPickApplication.java
+++ src/main/java/org/example/fantasypick/application/mainapp/FantasyPickApplication.java
@@ -3,7 +3,7 @@ package org.example.fantasypick.application.mainapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.example.fantasypick")
 public class FantasyPickApplication {
 
     public static void main(String[] args) {
